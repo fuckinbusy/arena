101ca9795bc9d98230259785c4903a391d656cba
richard <vasenev.231@mail.ru>
2026-01-08 11:57:58 +0800

    stress test added

--- arena_tests.c
+++ arena_tests.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <time.h>
 // #define ARENA_PLATFORM ARENA_PLATFORM_LIBC
 #define ARENA_IMPLEMENTATION
 #include "arena.h"
@@ -13,10 +14,23 @@ do {                                                               \
 #define ASSERT(cond)                                                       \
 do {                                                                      \
     if (!(cond)) {                                                          \
-        printf("\tAssertion failed: %s (%s:%d)\n", #cond, __FILE__, __LINE__);\
+        printf("!\tAssertion failed: %s (%s:%d)\n", #cond, __FILE__, __LINE__);\
         return false;                                                     \
     }                                                                     \
 } while (0)
+
+/// -------------------------------------
+static uint32_t rand_state;
+static inline void randinit()
+{
+    rand_state = (uint32_t)clock();
+}
+static inline uint32_t randrand()
+{
+    rand_state = rand_state * 0x333222 + 1013904223u;
+    return rand_state;
+}
+/// -------------------------------------
 
 TEST_CREATE(test_arena_create_destroy)
 {
@@ -139,13 +153,82 @@ TEST_CREATE(test_arena_grow_linear)
     return true;
 }
 
+TEST_CREATE(test_arena_stress)
+{
+    const uint32_t ITERATIONS = 1000000;
+
+    Arena arena = arena_create_ex(arena_config_create(
+        ARENA_CAPACITY_128MB,
+        0,
+        ARENA_ALIGN_CACHELINE,
+        ARENA_GROWTH_CONTRACT_FIXED,
+        ARENA_GROWTH_FACTOR_NONE,
+        ARENA_FLAG_DEBUG
+    ));
+
+    ASSERT(arena.base != NULL);
+    ASSERT(arena.max_capacity == arena.capacity);
+
+    arena_ptr_t arena_start  = (arena_ptr_t)arena.base;
+    arena_ptr_t arena_end    = arena_start + arena.capacity;
+    arena_size_t last_offset = arena.offset;
+
+    for (int i = 0; i < ITERATIONS; ++i) {
+        arena_size_t size = 1 + (randrand() % 0x1000);
+
+        size_t alignments[4] = {
+            ARENA_ALIGN_8B,
+            ARENA_ALIGN_16B,
+            ARENA_ALIGN_32B,
+            ARENA_ALIGN_64B, // same as ARENA_ALIGN_CACHELINE
+        };
+        size_t alignment = alignments[randrand()%4];
+
+        void *p = arena_alloc(&arena, size, alignment);
+
+        if (!p) break;
+
+        arena_ptr_t address = (arena_ptr_t)p;
+
+        ASSERT(address >= arena_start);
+        ASSERT(address + size <= arena_end);
+        ASSERT((address % alignment) == 0);
+        ASSERT(arena.offset >= last_offset);
+
+        arena_memset(p, (int)(i & 0xFF), size);
+
+        last_offset = arena.offset;
+    }
+
+    ASSERT(arena.debug.end != NULL);
+    ASSERT(arena.debug.total_allocations > 0);
+    ASSERT(arena.debug.total_allocations <= arena.capacity);
+    
+    arena_reset(&arena);
+
+    void *a = arena_alloc(&arena, 64, ARENA_ALIGN_DEFAULT);
+    ASSERT(a != NULL);
+    
+    arena_reset(&arena);
+    
+    void *b = arena_alloc(&arena, 64, ARENA_ALIGN_DEFAULT);
+    ASSERT(b != NULL);
+    
+    ASSERT(b == a);
+
+    arena_destroy(&arena);
+
+    return true;
+}
 
 int main(void)
 {
+    randinit();
     TEST_RUN(test_arena_create_destroy);
     TEST_RUN(test_arena_alloc);
     TEST_RUN(test_arena_alignment);
     TEST_RUN(test_arena_overflow);
     TEST_RUN(test_arena_reset);
     TEST_RUN(test_arena_grow_linear);
+    TEST_RUN(test_arena_stress);
 }
